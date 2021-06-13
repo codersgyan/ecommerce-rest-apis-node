5450ed1d80c4e46b52414011fc7212c8ec014af7
Coder's Gyan <codersgyan@gmail.com>
2021-06-13 22:18:14 +0300

    Add cors support

--- server.js
+++ server.js
@@ -5,20 +5,33 @@ const app = express();
 import routes from './routes';
 import mongoose from 'mongoose';
 import path from 'path';
+import cors from 'cors';
 
 // Database connection
-mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('DB connected...');
+    console.log('DB connected...');
 });
 
 global.appRoot = path.resolve(__dirname);
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', routes);
 app.use('/uploads', express.static('uploads'));
+app.use('/', (req, res) => {
+    res.send(`
+  <h1>Welcome to E-commerce Rest APIs</h1>
+  You may contact me <a href="https://codersgyan.com/links/">here</a>
+  Or You may reach out to me for any question related to this Apis: codersgyan@gmail.com
+  `);
+});
 
 app.use(errorHandler);
 const PORT = process.env.PORT || APP_PORT;
