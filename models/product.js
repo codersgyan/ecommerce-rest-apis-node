278a4473e8eed151243b43281de0edd27d95278f
Coder's Gyan <codersgyan@gmail.com>
2021-06-18 19:46:50 +0300

    Fix heroku image load issue

--- models/product.js
+++ models/product.js
@@ -2,14 +2,24 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 import { APP_URL } from '../config';
 
-const productSchema = new Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    size: { type: String, required: true },
-    image: { type: String, required: true, get: (image) => {
-        // http://localhost:5000/uploads/1616443169266-52350494.png
-        return `${APP_URL}/${image}`;
-    }},
-}, { timestamps: true, toJSON: { getters: true }, id: false });
+const productSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+        size: { type: String, required: true },
+        image: {
+            type: String,
+            required: true,
+            get: (image) => {
+                // http://localhost:5000/uploads/1616443169266-52350494.png
+                if (process.env.ON_HEROKU == 'true') {
+                    return `${image}`;
+                }
+                return `${APP_URL}/${image}`;
+            },
+        },
+    },
+    { timestamps: true, toJSON: { getters: true }, id: false }
+);
 
 export default mongoose.model('Product', productSchema, 'products');
